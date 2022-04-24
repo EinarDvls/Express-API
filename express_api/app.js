c279a5dc0502861388659e3a9e3868754fc52041
EinarDvls <launchx06534@innovaccion.mx>
2022-04-24 02:32:09 -0500

    Agregando GET para que regrese una lista de explorers

--- express_api/app.js
+++ express_api/app.js
@@ -1,4 +1,4 @@
-// Usando objetos express
+// Usando objeto express
 const express = require('express')
 // App de Express
 const app = express()
@@ -6,7 +6,19 @@ app.use(express.json()) // Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app: localhost:3000
 const port = 3000
 
+// HTTP METHODS
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers GET ALL requests ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer1 = {id: 1, name: "Einar"}
+    const explorer2 = {id: 2, name: "Einar"}
+    const explorer3 = {id: 3, name: "Einar"}
+    const explorer4 = {id: 3, name: "Einar"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    res.status(200).json(explorers)
+  })
+
 // Con esto inicializamos esta app
-app-listen(port, () => {
-    console.log('Example app listening on port ${port}')
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`)
 })
